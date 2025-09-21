9d67dd5c88feabfe3cc6dd22f3fe0f8a74141bc2
iamp1000 <dhingrapranavv@gmail.com>
2025-09-21 18:14:27 +0630

    general updates

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Replace 'your-repo-name' with the actual name of your GitHub repository
-  base: 'Gourav-Academy', 
+    base: '/Gourav-Academy/',
 })
